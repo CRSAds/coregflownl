83edf5c8f6bb72a3ae05f276e82a077990d544c6
CRSAds <yuri@crsadvertising.com>
2025-11-04 15:32:45 +0100

    Update register-visit.js

--- api/register-visit.js
+++ api/register-visit.js
@@ -2,7 +2,14 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") return res.status(200).end();
+  if (req.method === "OPTIONS") {
+  res.writeHead(200, {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, X-AUTH-SIGNATURE",
+  });
+  return res.end();
+}
   if (req.method !== "POST") return res.status(405).json({ error: "Method Not Allowed" });
 
   try {
